55e4cd9fef9cbb3fc931950acd9b798c4b0f2e61
Harsh18999 <158613066+Harsh18999@users.noreply.github.com>
2024-02-03 01:03:01 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+from datetime import datetime
+import numpy as np
+st.title("Shop Management App")
+st.write('''
+This app dedicated to Harsh. It is a shop management web app ,it Help to shopkeeper to help him to analysis his sell 
+         and store data. This app is under construction #Thank You''')
+st.markdown("""
+<style>
+        body{
+        background-color: #ff0000
+        }
+</style>
+            """
+        ,unsafe_allow_html=True)
