2db8957266652c422cfd13987a64e8310d04d577
Harsh18999 <158613066+Harsh18999@users.noreply.github.com>
2024-02-16 23:26:48 +0530

    Update test2.py

--- pages/test2.py
+++ pages/test2.py
@@ -1,3 +1,11 @@
 import sqlite3
-conn=sqlite3.connect("Database.db")
+import streamlit as st
+conn=sqlite3.connect("Darabase.db")
+query = '''
+            SELECT * FROM SELL
+    '''
+data=conn.execute(query)
+for n in data:
+    st.write(n)
+conn.commit()
 conn.close()
