6a1182b6c19bb1045cb29c4fb91b4e63ade7a0d2
Harsh18999 <158613066+Harsh18999@users.noreply.github.com>
2024-02-16 12:46:13 +0530

    Update database.py

--- pages/database.py
+++ pages/database.py
@@ -1,13 +1,11 @@
 import streamlit as st
 import sqlite3
 conn=sqlite3.connect("Darabase.db")
-ins='''
-       CREATE TABLE IF NOT EXISTS SIGNIN(
-       NAME VARCHAR(70),
-       EMAIL VARCHAR(70) PRIMARY KEY,
-       PASSWORD VARCHAR(70)
-       )
-'''
+ins=''' 
+         INSERT INTO SIGNIN(NAME,EMAIL,PASSWORD) VALUES
+                ("HARSH","MYGMAIL.COM","PASSWORD") 
+         
+    '''
 data=conn.execute(ins)
 st.title('Harsh')
 for n in data:
