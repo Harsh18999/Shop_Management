c546dd5005e9a922c4aee8c83c7b33faf837de7b
Harsh18999 <158613066+Harsh18999@users.noreply.github.com>
2024-02-16 22:44:31 +0530

    Add files via upload

---
+++ pages/database.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import sqlite3
+conn=sqlite3.connect("Darabase.db")
+data=conn.execute("SELECT NAME FROM SIGNIN")
+st.title('Harsh')
+for n in data:
+    st.write(n)
+conn.close()
