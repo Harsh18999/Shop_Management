b6a60e5e9ebb53132e4fb91c924472193adc97d5
Harsh18999 <158613066+Harsh18999@users.noreply.github.com>
2024-02-16 12:58:53 +0530

    Update database.py

--- pages/database.py
+++ pages/database.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import sqlite3
 conn=sqlite3.connect("Darabase.db")
-ins=''' 
-         SELECT * FROM SIGNIN
+INS= ''' 
+         INSERT INTO SIGNIN(NAME,EMAIL,PASSWORD) VALUES
+                ("HARSH","MYGMAIL.COM","PASSWORD") 
          
     '''
-data=conn.execute(ins)
+data=conn.execute(INS)
 st.title('Harsh')
 for n in data:
     st.write(n)
