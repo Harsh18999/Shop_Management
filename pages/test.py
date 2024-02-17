a8d832e2d0d9959390b5f5156a1c794c4df7d8ef
Harsh18999 <158613066+Harsh18999@users.noreply.github.com>
2024-02-17 18:55:56 +0530

    Add files via upload

---
+++ pages/test.py
@@ -0,0 +1,21 @@
+import sqlite3
+import streamlit as st
+import pandas as pd
+df=pd.DataFrame()
+l=["CUSTOMER_NAME","ORDER_NAME","PRICE","ORDER_ID","DATE","TIME"]
+conn=sqlite3.connect("Darabase.db")
+st.title('''HEY FRIEND I AM HARSH 👋''')
+st.write("THIS IS TODAY SELL DATA")
+for n in l:
+    query = f'''
+            SELECT {n} FROM SELL
+    '''
+    data=conn.execute(query)
+    p=[]
+    for k in data:
+        for m in list(k):
+            p.append(m)
+    df[n]=p
+    conn.commit()
+st.dataframe(df)
+conn.close()
