757adfad84eecb72f61e3bd7e4d4d14247b4cfce
Harsh18999 <158613066+Harsh18999@users.noreply.github.com>
2024-02-17 18:59:54 +0530

    Update Add_New_Order.py

--- pages/Add_New_Order.py
+++ pages/Add_New_Order.py
@@ -7,7 +7,7 @@ current_date_time = datetime.now()
 st.title("New custmor")
 name=st.text_input("Enter custmor name")
 order=st.text_input("Enter order name")
-id=int(st.number_input("Enter Order Id"))
+id=int(st.text_input("Enter Order Id"))
 price=int(st.number_input("Enter total amount"))
 if st.button("Submit"):
     new_data=(str(name),str(order),price,str(id),str(current_date_time.date()),str(datetime.now().strftime("%I")+":"+datetime.now().strftime("%M")+" "+datetime.now().strftime("%p")))
