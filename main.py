3b785856e6a28bc9855ee0df9802a12ef09aad58
Harsh18999 <158613066+Harsh18999@users.noreply.github.com>
2024-02-19 01:09:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 from datetime import datetime
 import numpy as np
-st.title("Shop Management App")
+st.title('''HEY FRIEND I AM :blue[HARSH] 👋''')
 st.write('''
 This app dedicated to Harsh. It is a shop management web app ,it Help to shopkeeper to help him to analysis his sell 
          and store data. This app is under construction #Thank You''')
