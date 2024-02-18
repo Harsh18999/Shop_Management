98beb0027240e16e5118a209d111ffd3abd112f1
Harsh18999 <158613066+Harsh18999@users.noreply.github.com>
2024-02-19 01:45:03 +0530

    Add files via upload

---
+++ pages/test2.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# Initialize connection.
+conn = st.connection('mysql', type='sql')
+
+# Perform query.
+df = conn.query('SELECT PRODUCT_NAME from TEST1', ttl=600)
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row} has a :{row}:")
