4cf94d3ef0f95af223464f83f403094d6d8375f7
Harsh18999 <158613066+Harsh18999@users.noreply.github.com>
2024-02-21 16:47:28 +0530

    Update Add_New_Order.py

--- pages/Add_New_Order.py
+++ pages/Add_New_Order.py
@@ -1,3 +1,4 @@
+from sqlalchemy import text
 conn = st.connection('mysql', type='sql')
 current_date_time = datetime.now()
 st.title("New custmor")
@@ -6,3 +7,8 @@ order=st.text_input("Enter order name")
 id=st.text_input("Enter Order Id")
 price=int(st.number_input("Enter total amount"))
 if st.button("Submit"):
+    new_data=(str(name),str(order),price,str(id))
+    with conn.session as s:
+        s.execute(text(f'INSERT INTO TEST1(CUSTOMER_NAME,PRODUCT_NAME,PRICE,ORDER_ID) VALUES {new_data}'))
+        s.commit()
+        st.success("Succesfully Saved, Thank you")
