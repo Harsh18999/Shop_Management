df9dbda672d8642fab7358fdd424aa6d1b240a22
Harsh18999 <158613066+Harsh18999@users.noreply.github.com>
2024-03-16 16:17:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,59 @@ import SIGNIN
 import functions
 import bill_functions
 
-if SIGNIN.variable.login_status==False:
+def login():
+    st.title("Log In Page")
+    form=st.form("Log In")
+    user_name = form.text_input("Enter Your User_Name")
+    email=form.text_input('Enter Your Email')
+    password = form.text_input("Enter Your Password")
+    submitted=form.form_submit_button("Submit")
+    if submitted:
+        if len(user_name)!=0:
+            if len(email)!=0:
+                 if len(password)!=0:
+                     if SIGNIN.password_and_username_validator(password,email,user_name):
+                        try:   
+                            st.success("THANK YOU ")
+                            st.session_state.login_status=True
+                            st.session_state.username=user_name
+                            st.session_state.email=email
+                            st.session_state.bill_status=False
+                        except:
+                            st.error('SOME ERROR ...PLEASE TRY AGAIN')
+                 else:
+                     st.warning('Please Enter Your Password')
+            else :
+                st.warning("Please Enter Your Email")
+        else:
+            st.warning("PLEASE ENTER YOUR NAME")
+    else:
+        st.write(':blue[PLEASE ENTER DETAILS]')
+
+
+def change_password(email):
+    form=st.form("Change Password")
+    form.header('Reset Password')
+    ps = form.text_input("Enter Your Password")
+    cps = form.text_input("Re-enter Your Password")
+    submitted=form.form_submit_button("Submit")
+    if submitted:
+        if ps==cps:
+            try:
+                obj = psycopg2.connect("postgresql://MYPROJECT20.COM:ZNfo9DxeFp-WoNzpTDJPmg@almond-heron-1166.j77.cockroachlabs.cloud:26257/project?sslmode=verify-full")
+                cursor=obj.cursor()
+                cursor.execute(f''' UPDATE USERS
+                                    SET PASSWORD = '{cps}'
+                                    WHERE EMAIL = '{email}'
+                                ''')
+                obj.commit()
+                obj.close()
+                st.success('PASSWORD CHANGE SUCCESSFULLY')
+            except:
+                st.error('THIS USER IS NOT EXIST')
+        else:
+            st.warning('Password is not same ! Please check')
+if 'login_status' not in st.session_state:
     st.sidebar.title('Shoap Management App')
     selected_option=st.sidebar.selectbox('DATA',['Menu','Login','Signin'])
     if selected_option=='Menu':
@@ -15,7 +67,7 @@ if SIGNIN.variable.login_status==False:
                   THIS APP DEDICATED TO THE HARSH . THIS APP PERPOSE IS GIVE SOME ANALYTICAL HELP TO SHOP KEEPS TO ANALYSIS HIS CUSTOMER DATA AND SELL DATA :blue[Thank You]''')
         st.write('Please login to access our facilities')
     elif selected_option=='Login':
-        SIGNIN.login()
+        login()
     elif selected_option=='Signin':
         SIGNIN.Signin()
 else:
@@ -32,26 +84,45 @@ else:
     if selected_option_2=='Not Selected':
         st.write()
     if selected_option_2=='ADD NEW CUTOMER':
-        if bill_functions.variable.bill_status==False:
+        if st.session_state.bill_status==False:
             col1, col2= st.columns(2)
             with col1:
                 bill_functions.add_new_order()
             with col2:    
                 bill_functions.selected_products()
+                added_product=[]
+                quantity_product=[]
+                price_product=[]
+                dates=[]
+                times=[]
+                for n in bill_functions.product_list():
+                    added_product.append(n[0])
+                    quantity_product.append(n[1])
+                    price_product.append(n[2])
+                    dates.append(n[3])
+                    times.append(n[4])
+                bill=st.button('Generate Your Bill')
+                if bill:
+                    for n in range(len(bill_functions.product_list())):
+                        p={"description": f"{added_product[n]}", "quantity": quantity_product[n], "unit_price": price_product[n]/quantity_product[n] ,"amount": price_product[n],'discount':'5%'}
+                        bill_functions.add_item(p)
+                        st.session_state.bill_status=True
+                 
+                
         else:
             bill_functions.main()
             back=st.button('Back')
             if back:
-                bill_functions.variable.bill_status=False
+                st.session_state.bill_status=False
         
     if selected_option_2=='TOTAL SELL':
-        obj = psycopg2.connect("postgresql://MYPROJECT20.COM:ZNfo9DxeFp-WoNzpTDJPmg@almond-heron-1166.j77.cockroachlabs.cloud:26257/project?sslmode=require&sslrootcert=root.crt")
+        obj = psycopg2.connect("postgresql://MYPROJECT20.COM:ZNfo9DxeFp-WoNzpTDJPmg@almond-heron-1166.j77.cockroachlabs.cloud:26257/project?sslmode=verify-full")
         cursor=obj.cursor()
         df=pd.DataFrame()
         l=["CUSTOMER_NAME","ORDER_NAME","PRICE","ORDER_ID","DATE","TIME"]
         st.subheader('''Your Total Sell Data''')
         for n in l:
-            query = f'SELECT {n} FROM {SIGNIN.variable.username}_ALL_DATA'
+            query = f'SELECT {n} FROM {st.session_state.username}_ALL_DATA'
             cursor.execute(query)
             result=cursor.fetchall()
             p=[]
